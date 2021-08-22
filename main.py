5bc372bd37989739a515e5f37fb69df6d48c44dc
Dekonds <and.Dekond@gmail.com>
2021-08-22 13:15:59 +0300

    test

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
+coutofparser = 99
+no_visits = True
 
 HEADERs = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4297.0 Safari/537.36',
@@ -12,17 +14,19 @@ HEADERs = {
 
 
 def get_html(url, params=None):
+    f = open('cfg', 'r')
+    strings = f.read()
+    splitss = strings.split("\n")
+    global coutofparser
     s = requests.Session()
-    # print(url)
     r = s.get(url, \
               cookies={
-                    'session_id': '8FBABDE6-234F-1F13-FB17-053A557F4BCC', \
-                    'user_data': 'a%3A0%3A%7B%7D', \
-                    'secret': '8595BB40-D44C-BE89-3A53-2FCE2D95184F'},
-            headers=HEADERs
+                  'session_id': '8FBABDE6-234F-1F13-FB17-053A557F4BCC',  #'1952AD09-578E-FCDB-62BC-C71C86E74CBC', splitss[coutofparser].split(" ")[2]
+                  'secret': '8595BB40-D44C-BE89-3A53-2FCE2D95184F'      #'EEC5222B-AEFE-96B3-9125-1928512E7C5B' splitss[coutofparser].split(" ")[3]
+                  },
+              headers=HEADERs
               )
     return r
-
 
 def get_html_ip(url, params=None):
     s = requests.Session()
@@ -36,8 +40,13 @@ def get_content(html):
     soup = BeautifulSoup(html, 'html.parser')
     classes = soup.find_all("div", class_="left")
     now = datetime.datetime.now()
+    global no_visits
     if (len(classes) == 0):
-        print(str(now.hour) + ":" + str(now.minute) + ":" + str(now.second) + " No visits")
+        if (no_visits == True):
+            print(str(now.hour) + ":" + str(now.minute) + ":" + str(now.second) + " No visits")
+            no_visits = False
+        else:
+            print("visits false")
         time.sleep(200)
         gc.collect()
         return
@@ -51,11 +60,8 @@ def get_content(html):
         print(str(now.hour) + ":" + str(now.minute) + ":" + str(now.second) + " " + link + " " + typeoflink)
         get_html(link + "&act=redirect")
         time.sleep(int(typeoflink) + 10)
-    # get_html(link + "&act=redirect")
-    # print("Memory " + str(memory_usage()))
+    no_visits = True 
     gc.collect()
-    # parse()
-    # f = open('parse.html', 'w', encoding='utf-8')
 
 
 def get_content_username(html):
@@ -75,19 +81,19 @@ def parse():
 def getip():
     html = get_html_ip("https://ipwhois.app/json/")
     ip_json = json.loads(html.text)
-    print(ip_json['latitude'])
-    print(html.text.split('"')[3])
+    print("asn: "+str(ip_json['asn']))
+    print('ip: '+ip_json['ip'])
     f = open('cfg', 'r')
     strings = f.read()
     cfg_file = strings.split("\n")
     for s in cfg_file:
         file_line = s.split(" ")
-        # print(file_line[0]+" "+str(ip_json['latitude']))
-        # print(str(file_line[0]) == str(ip_json['latitude']))
-        if (file_line[0] in str(ip_json['latitude'])):
+        print(file_line[0]+" "+str(ip_json['asn']))
+        if (file_line[0] in str(ip_json['asn'])):
             global coutofparser
             coutofparser = cfg_file.index(s)
             print("Logging as " + file_line[1])
+            break
     html2 = get_html('https://socpublic.com/account/visit.html')
     get_content_username(html2.text)
 
